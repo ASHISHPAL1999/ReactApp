886b9c2e88d296062ae62d3bc5b79817b81bd38f
ASHISH PAL <68744952+ASHISHPAL1999@users.noreply.github.com>
2021-03-14 17:17:11 +0530

    Updates

---
+++ precache-manifest.51d3a86acc753a5ad11928b4b80c7dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f83d5765e5d7a44123d75e6007cb8c",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "7e5b6d4fb538714713ee",
+    "url": "/ReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1861578918bc0bd5585",
+    "url": "/ReactApp/static/js/2.3ba5a1f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.3ba5a1f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5b6d4fb538714713ee",
+    "url": "/ReactApp/static/js/main.5c4e8071.chunk.js"
+  },
+  {
+    "revision": "ad4c4c9d171457729625",
+    "url": "/ReactApp/static/js/runtime-main.86e31f41.js"
+  }
+]);
